7b05146f6a4f5b14b13fcbe7cfa078167784aeed
Mikael Bendiksen <duuxman@gmail.com>
2012-05-03 16:41:19 +0200

    Tower of Hanoi

---
+++ tower/src/SolveTowers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//********************************************************************
+//  SolveTowers.java       Author: Lewis/Loftus
+//
+//  Demonstrates recursion.
+//********************************************************************
+
+public class SolveTowers
+{
+   public static void main (String[] args)
+   {
+	   int input;
+	   Scanner scan = new Scanner (System.in);
+		
+	   System.out.println("Totale brikker: ");
+	   input = scan.nextInt();
+	   TowersOfHanoi towers = new TowersOfHanoi (input);
+	   System.out.println("\nFlytteinstruksjon\n---------------------------\n");
+	   
+	   towers.solve();
+	   
+	   System.out.println("\nTotalt flyttninger: " + towers.movecount);
+   }
+}
